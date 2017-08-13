f98955a06608bbc5423c84bdf4e3c94cbbb03863
Hugo Sadok <hugombarreto@gmail.com>
2017-08-13 16:29:18 -0300

    Using setuptools

--- guiabolso2csv/__init__.py
+++ guiabolso2csv/__init__.py
@@ -2,4 +2,4 @@
 
 __author__ = 'Hugo Sadok'
 __email__ = 'hugo@sadok.com.br'
-__version__ = '1.0.3
+__version__ = '0.2'
--- setup.py
+++ setup.py
@@ -1,13 +1,36 @@
-from distutils.core import setup
+# coding=utf-8
+from setuptools import setup, find_packages
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
 
 setup(
     name='guiabolso2csv',
-    version='',
-    packages=[''],
-    url='',
-    license='',
+    version='0.2',
+    description="GuiaBolso2csv is a simple Python program that can be used to"
+                "download GuiaBolso transactions in a csv format.",
+    long_description=readme,
+    packages=find_packages(),
+    url='https://github.com/hugombarreto/guiabolso2csv',
+    license='GPLv3',
     author='Hugo Sadok',
-    author_email='',
-    description='',
-    requires=['requests','click','unicodecsv']
+    author_email='hugo@sadok.com.br',
+    entry_points={
+        'console_scripts': [
+            'guiabolso2csv=guiabolso2csv.__main__:main'
+        ]
+    },
+    include_package_data=True,
+    install_requires=[
+        'requests',
+        'click',
+        'unicodecsv'
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 2.7',
+    ],
 )
